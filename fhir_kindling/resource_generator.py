e982453d2c5081cbde0bf037cfa0022bef3da23e
Michael Graf <michael.graf3110@gmail.com>
2021-08-12 15:13:02 +0200

    upload functionality for base resource generator

--- fhir_kindling/resource_generator.py
+++ fhir_kindling/resource_generator.py
@@ -1,33 +1,38 @@
 import pprint
 from typing import List, Union
+
+import requests
 from fhir.resources.domainresource import DomainResource
 from fhir.resources.bundle import Bundle, BundleEntry, BundleEntryRequest
 import os
-
-from train_lib.fhir import PHTFhirClient
+import pendulum
+from requests.auth import HTTPBasicAuth
 
 
 class FhirResourceGenerator:
 
     def __init__(self, n: int, resources: List[DomainResource] = None,
                  resource_type: DomainResource = None,
-                 fhir_server: str = None, fhir_user: str = None, fhir_pw: str = None, fhir_token: str = None):
+                 fhir_server: str = None, fhir_user: str = None, fhir_pw: str = None, fhir_token: str = None,
+                 fhir_server_type: str = None):
         self.fhir_token = fhir_token if fhir_token else os.getenv("FHIR_TOKEN")
         self.fhir_pw = fhir_pw if fhir_pw else os.getenv("FHIR_PW")
         self.fhir_user = fhir_user if fhir_user else os.getenv("FHIR_USER")
         self.fhir_server = fhir_server if fhir_server else os.getenv("FHIR_SERVER_URL")
+        self.fhir_server_type = fhir_server_type if fhir_server_type else os.getenv("FHIR_SERVER_TYPE")
         self.n = n
         self.resource_type = resource_type
         self.resources = resources
 
-        if self.fhir_server:
-            self.fhir_client = PHTFhirClient(server_url=self.fhir_server, username=self.fhir_user,
-                                             password=self.fhir_pw, token=self.fhir_token)
-
-    def generate(self, upload: bool = False):
+    def generate(self, upload: bool = False, out_dir: str = None):u
+        bundle = self.make_bundle()
         if upload:
-            bundle = self.make_bundle()
-            self.fhir_client.upload_resource_or_bundle(bundle=bundle)
+            self.upload_resource_or_bundle(bundle=bundle)
+
+        if out_dir:
+            path = os.path.join(out_dir, f"bundle-{pendulum.now().isoformat()}.json")
+            with open(path, "w") as bundle_file:
+                bundle_file.write(bundle.json())
 
     def make_bundle(self) -> Bundle:
         entries = self._generate_bundle_entries()
@@ -49,5 +54,65 @@ class FhirResourceGenerator:
             entries.append(entry)
         return entries
 
+    def upload_resource_or_bundle(self, resource=None, bundle: Bundle = None):
+        auth = self._generate_auth()
+        api_url = self._generate_api_url()
+        if bundle:
+            self._upload_bundle(bundle=bundle, api_url=api_url, auth=auth)
+        if resource:
+            # TODO upload single resource
+            pass
+
+    def _generate_api_url(self) -> str:
+        url = self.fhir_server
+        if self.fhir_server_type == "ibm":
+            url += "/fhir-server/api/v4"
+
+        elif self.fhir_server_type in ["blaze", "hapi"]:
+            url += "/fhir"
+
+        else:
+            raise ValueError(f"Unsupported FHIR server type: {self.fhir_server_type}")
+
+        return url
+
+    def _upload_bundle(self, bundle: Bundle, api_url: str, auth: requests.auth.AuthBase):
+        headers = self._generate_bundle_headers()
+        r = requests.post(api_url, auth=auth, data=bundle.json(), headers=headers)
+        r.raise_for_status()
+
+    def _generate_bundle_headers(self):
+        headers = {}
+        if self.fhir_server_type == "blaze":
+            headers["Content-Type"] = "application/fhir+json"
+
+        else:
+            # todo figure out if other servers require custom headers for bundle upload
+            headers["Content-Type"] = "application/fhir+json"
+
+        return headers
+
     def display_schema(self):
         pprint.pprint(self.resource_type.schema())
+
+    def _generate_auth(self) -> requests.auth.AuthBase:
+        """
+        Generate authoriation for the request to be sent to server. Either based on a given bearer token or using basic
+        auth with username and password.
+
+        :return: Auth object to pass to a requests call.
+        """
+        if self.fhir_user and self.fhir_pw:
+            return HTTPBasicAuth(username=self.fhir_user, password=self.fhir_pw)
+        # TODO request token from id provider if configured
+        elif self.fhir_token:
+            return BearerAuth(token=self.fhir_token)
+
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
