822823063b6b1894497d7d5f9d7dc1ad50cca593
migraf <Eufnayg1!>
2021-08-20 11:29:04 +0200

    initial yml files

--- fhir_kindling/cli.py
+++ fhir_kindling/cli.py
@@ -1,14 +1,19 @@
 """Console script for fhir_kindling."""
 import sys
 import click
+import yaml
 
 
 @click.command()
-def main(args=None):
+@click.option("--file", default="kindling.yml", help="Path to the .yml file defining the generation specs.")
+def main(file):
     """Console script for fhir_kindling."""
     click.echo("Replace this message by putting your code into "
                "fhir_kindling.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+    click.echo(f"kindling file: {file}")
+
+
     return 0
 
 
