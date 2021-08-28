eea4a12db722c58486c56236c95d7ab3592fa863
migraf <Eufnayg1!>
2021-08-28 10:47:24 +0200

    Bump version: 0.1.0 → 0.2.0

--- fhir_kindling/__init__.py
+++ fhir_kindling/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Michael Graf"""
 __email__ = 'michael.graf@uni-tuebingen.de'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .upload import upload_bundle, upload_resource
 from .delete import delete_resources
