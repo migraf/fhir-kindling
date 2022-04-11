5378500b789516896f425c135ce8bb247b6ed6ed
migraf <michael.graf3110@gmail.com>
2022-04-11 10:52:50 +0200

    Bump version: 0.8.0 → 0.8.1

--- fhir_kindling/__init__.py
+++ fhir_kindling/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Michael Graf"""
 __email__ = 'michael.graf@uni-tuebingen.de'
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 from .fhir_server import FhirServer
 from .fhir_query import FHIRQuery
