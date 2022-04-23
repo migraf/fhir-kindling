0eb1fc7fbcb24ad4618e7296c7755641f76c1a01
migraf <michael.graf3110@gmail.com>
2022-04-23 15:29:09 +0200

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/migraf/fhir-kindling",
-    version='0.8.1',
+    version='0.8.2',
     zip_safe=False,
 )
