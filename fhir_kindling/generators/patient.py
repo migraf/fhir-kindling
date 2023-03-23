ea9be56deae9776c53b7c4694b7858df6ee7e9d1
Michael Graf <michael.graf3110@gmail.com>
2023-03-23 09:12:12 +0100

    feat: use faker for name generation in generators

--- fhir_kindling/generators/patient.py
+++ fhir_kindling/generators/patient.py
@@ -4,11 +4,10 @@ from uuid import uuid4
 
 import pandas as pd
 import pendulum
+from faker import Faker
 from fhir.resources.humanname import HumanName
 from fhir.resources.patient import Patient
 from fhir.resources.reference import Reference
-
-from fhir_kindling.generators.names import FIRST_NAMES, LAST_NAMES
 
 
 class PatientGenerator:
@@ -73,10 +72,14 @@ class PatientGenerator:
 
     @staticmethod
     def _generate_patient_names(n: int):
-        first_names = random.choices(FIRST_NAMES, k=n)
-        last_names = random.choices(LAST_NAMES, k=n)
-        names = list(zip(first_names, last_names))
+        fake = Faker()
 
+        names = []
+        for _ in range(n):
+            split = fake.name().split(" ")
+            family = split[-1]
+            given = " ".join(split[:-1])
+            names.append((given, family))
         return names
 
     def _generate_birthdate(self):
