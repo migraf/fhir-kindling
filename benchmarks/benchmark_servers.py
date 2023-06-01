6dba5e28a717b3267ffb0a2704175bffa02b8ccb
Michael Graf <michael.graf3110@gmail.com>
2023-06-01 07:24:37 +0200

    integrate l4h fhir server into benchmark script

---
+++ benchmarks/benchmark_servers.py
@@ -0,0 +1,44 @@
+from fhir_kindling import FhirServer
+from fhir_kindling.util.benchmark import ServerBenchmark
+
+DEFAULT_SERVERS = [
+    {"name": "blaze", "api_address": "http://localhost:9090/fhir"},
+    {"name": "hapi", "api_address": "http://localhost:9091/fhir"},
+    {
+        "name": "linux4h",
+        "api_address": "http://localhost:9080/fhir-server/api/v4/",
+        "credentials": {"username": "fhiruser", "password": "change-password"},
+    },
+]
+
+
+def run_benchmark(servers=DEFAULT_SERVERS):
+    """Run a benchmark against the given servers."""
+
+    # initialize server objects
+    print("initializing servers")
+    benchmark_servers = []
+    for s in servers:
+        print(s)
+        print(f"initializing Server {s['name']} -- {s['api_address']}")
+        credentials = s.get("credentials", None)
+        if credentials:
+            benchmark_servers.append(
+                FhirServer(
+                    api_address=s["api_address"],
+                    **credentials,
+                )
+            )
+        else:
+            benchmark_servers.append(FhirServer(api_address=s["api_address"]))
+
+    print("running benchmark")
+
+    benchmark = ServerBenchmark(servers=benchmark_servers)
+    benchmark.run_suite()
+    figure = benchmark.plot()
+    figure.show()
+
+
+if __name__ == "__main__":
+    run_benchmark()
